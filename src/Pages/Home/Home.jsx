7b2cf68ebfae7fbed837b5bdf7ca62b0fbe84426
israrahmadtech <israrahmadtech@gmail.com>
2025-10-21 22:10:00 +0500

    feature/categories and feature/footer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,25 +1,14 @@
 import { Link } from 'react-router-dom'
 import products from '../../products'
+import Categories from '../../Components/Categories/Categories'
 
 function Home() {
   
   
   return (
-    <div className=''>
-      <div className="flex">
-        {products?.map((product) => (
-          <Link to={ `/products/${product.id}`}key={product?.id}>
-            <div >
-              <img src={product?.image} alt="" />
-              <h3>{product?.name}</h3>
-              <h4>{product?.category}</h4>
-              <p className="text-yellow-500">⭐ {product.rating}</p>
-              <p>{product?.description}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <>
+      <Categories/>
+    </>
   )
 }
 

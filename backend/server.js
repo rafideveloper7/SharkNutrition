46329b1687fa030683021878ca834507f1d68b5f
israrahmadtech <israrahmadtech@gmail.com>
2025-11-06 19:44:07 +0500

    Some changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // =============== MIDDLEWARE ===============
 app.use(cors({
-  origin: "http://localhost:5173", // frontend origin
+  origin: "https://sharknutrition.store", // frontend origin
   credentials: true,               // allow cookies
 }));
 app.use(bodyParser.json());

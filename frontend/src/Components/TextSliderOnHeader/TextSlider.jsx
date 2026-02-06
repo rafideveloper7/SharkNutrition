9a9fda2f6c64543845c821706c8ae18f1617c7fd
Rafi Ullah <rafideveloper7@gmail.com>
2026-02-06 21:53:02 +0500

    add email services

--- frontend/src/Components/TextSliderOnHeader/TextSlider.jsx
+++ frontend/src/Components/TextSliderOnHeader/TextSlider.jsx
@@ -3,8 +3,8 @@ import Marquee from "react-fast-marquee";
 
 function TextSlider() {
   return (
-    <div className="w-full bg-blue-400 h-[30px] lg:h-[45px] overflow-hidden flex justify-around">
-      <Marquee speed={80} pauseOnHover gradient={false}>
+    <div className="w-full bg-blue-400 h-[30px] lg:h-[45px] overflow-hidden flex justify-around ">
+      <Marquee speed={80} pauseOnHover gradient={false} className="overflow-hidden">
         <div className="flex gap-80 px-16 m-36 sm:m-18">
           <span className="font-bold">DIVE INTO AUTHENTICITY</span>
           <span className="font-bold">7-DAY MONEY-BACK GUARANTEE</span>
